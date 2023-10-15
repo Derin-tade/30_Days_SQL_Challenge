7ae8a961d430367aa2caf293c8ebcedaadcff967
Derin-tade <eyitaderinsola@gmail.com>
2023-10-15 23:33:30 +0100

    updated day5 with new methods

--- 30Day_techavilly/day5query.sql
+++ 30Day_techavilly/day5query.sql
@@ -19,3 +19,35 @@ select*from pizza_names;
 select*from customer_orders;
 select*from pizza_recipes;
 select*from runners;
+
+-- Using Subqueries
+select 
+	pizza_name, 
+	COUNT(*) as pizza_deliveries
+from 
+	customer_orders co 
+	join 
+	pizza_names pz 
+	on 
+	co.pizza_id=pz.pizza_id 
+where co.order_id in (select ro.order_id
+						from runner_orders ro 
+						where ro.cancellation is null)
+group by pizza_name;
+
+
+-- Using CTE
+with pizruncust as 
+(select pz.pizza_id, pz.pizza_name from pizza_names pz
+left join
+customer_orders co on
+pz.pizza_id=co.pizza_id
+join
+runner_orders ro on
+co.order_id=ro.order_id
+where ro.cancellation is null)
+
+select 
+pizza_name, count(*) as pizza_deliveries from pizruncust
+group by pizza_name
+order by pizza_name;
